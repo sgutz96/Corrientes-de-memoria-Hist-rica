7d388571a7e46b8a04194214ed9f5c25e3fd4f16
AV <apvanegasg@academia.usbbog.edu.co>
2025-03-20 12:33:22 -0500

    V19_03

--- JS/productos.js
+++ JS/productos.js
@@ -1,13 +1,15 @@
-document.getElementById("descargarBtn").addEventListener("click", function() {
-
-    
-    const apkUrl = "URL_DEL_APK";
-    const a = document.createElement("a");
-    a.href = apkUrl;
-    a.download = "Juego.apk";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-
-
+document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", function() {
+        const apkUrl = this.getAttribute("data-apk"); 
+        if (apkUrl) {
+            const a = document.createElement("a");
+            a.href = apkUrl;
+            a.setAttribute("download", apkUrl);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        } else {
+            alert("No hay un archivo APK disponible para esta tarjeta.");
+        }
+    });
 });
