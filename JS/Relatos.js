e3045d5cf4eb0dc6c688cc9ed535daf2bccbd2ed
sgutz96 <sgutz0527@gmail.com>
2025-04-03 21:29:07 -0500

    Descripción de los cambios realizados

--- JS/Relatos.js
+++ JS/Relatos.js
@@ -2,6 +2,17 @@
 var nombre = "";
 var rango = "";
 var text = "";
+
+
+function colorCirculos(circuloActive){
+       // Resetear todos los círculos a blanco
+       document.querySelectorAll(".circle").forEach(circle => {
+        circle.style.background = "#FAFAFA"; // Blanco
+    });
+
+    document.querySelectorAll(".circle")[circuloActive - 1].style.backgroundColor = "#304D3D";
+
+}
 function mostrarAlerta(circuloId) {  
 
     var video = document.getElementById('relatosVideo');
@@ -10,43 +21,47 @@ function mostrarAlerta(circuloId) {
     
     switch (circuloId) {
         case 0:
-           
+            colorCirculos(circuloId);
         break;
         case 1:
             //alert("Has hecho clic en el círculo número " + circuloId);
-            nombre = "Carlos Arbey Claros"
-            rango = "rango1"
+            nombre = "Arnold"
+            rango = "Comunidad Apapois"
             text = "Pues la armada nacional es la empresa más importante que tiene este municipio, la armada nacional es la que le genera soberanía nacional... ";
 
             // Cambiar la fuente del video
-            videoSource.src = './Assets/Videos/VideoBanner.mp4';
+            videoSource.src = './Assets/Videos/Relatos/Arnold comunidad apapois.webm';
+            colorCirculos(circuloId);
         break;
         case 2:
             //alert("Has hecho clic en el círculo número " + circuloId);
-            nombre = "Arturo Suleta"
-            rango = "Poblador"
+            nombre = "Erika Paula cruz gaitan"
+            rango = "Gobernadora Resguardo indigena Lagarto Cocha"
             text = "De esta región es el eje, créeme que ellos pues la fuente de trabajo, pues cuando ha habido coca, pues la vaina de la gente con su coca, o su oro, pero es el ...";
 
             // Cambiar la fuente del video
-            videoSource.src = './Assets/Videos/VideoBanner.mp4';
+            videoSource.src = './Assets/Videos/Relatos/Erika Paula cruz gaitan Gobernadora.mp4';
+            colorCirculos(circuloId);
         break;
         case 3:
             //alert("Has hecho clic en el círculo número " + circuloId);
-            nombre = "Carlos Eduardo Siniestra"
-            rango = "rango3"
+            nombre = "Fernando trujillo"
+            rango = "Fundacion Omacha"
             text = "La jurisdicción del municipio cuenta con 87 km del río, donde se encuentran dispersas diecinueve comunidades indígenas con necesidades distintas a la capital...";
 
             // Cambiar la fuente del video
-            videoSource.src = './Assets/Videos/VideoBanner.mp4';
+            videoSource.src = './Assets/Videos/Relatos/Fernando trujillo.webm';
+            colorCirculos(circuloId);
         break;
         case 4:
             //alert("Has hecho clic en el círculo número " + circuloId);
-            nombre = "David García Becerra"
-            rango = "rango1"
+            nombre = "Viana makiscara quipa"
+            rango = "Gobernadora Resguardo indigena Lagarto Cocha"
             text = "Hemos ganado terreno con la comunidad, nos hemos dado a querer, en el sentido de que promovemos la navegación segura. Iniciamos la campaña 'Cuida tu vida, navega seguro'...";
 
             // Cambiar la fuente del video
-            videoSource.src = './Assets/Videos/VideoBanner.mp4';
+            videoSource.src = './Assets/Videos/Relatos/Viana makiscara quipa.webm';
+            colorCirculos(circuloId);
         break;
         case 5:
             
@@ -55,7 +70,12 @@ function mostrarAlerta(circuloId) {
         
         break;
 
-        default:        
+        default:   
+        
+    
+
+        // Cambiar el color del círculo seleccionado a verde
+       
             
         }
 
